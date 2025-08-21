36f32d29f0a0b438cc8d8d5e99e88a0e823ebad6
Husnain <Husnain.tayab@hotmail.com>
2025-08-21 16:47:17 +0500

    Largest value

---
+++ function_03.py
@@ -0,0 +1,22 @@
+
+
+def greater (number1 ,number2, number3):
+
+    if(number1>number2):
+
+        return f"THE LARGEST NUMBER IS :{number1}"
+    
+    elif(number2>number3):
+     return f"THE LARGEST NUMBER IS :{number2}"
+
+    elif(number3>number1):
+
+        return f"THE LARGEST NUMBER IS :{number3}"
+    
+number1= int(input("Enter your number : "))
+
+number2= int(input("Enter your number : "))
+
+number3= int(input("Enter your number : "))
+
+print(greater(number1,number2,number3))
