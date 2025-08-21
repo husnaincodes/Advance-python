c29720147bc5fbd2e7000c8ddc1c424255f56f90
Husnain <Husnain.tayab@hotmail.com>
2025-08-21 16:20:32 +0500

    Function-Return

---
+++ funtion_02.py
@@ -0,0 +1,15 @@
+
+def GoodDay(name, ending = "Thank you!"):
+
+    print(f"Good Day,{name}")
+
+    print(ending)
+
+    return "done" 
+
+a = GoodDay("User", "Thanks")
+
+GoodDay("User2")
+
+
+print(a)
