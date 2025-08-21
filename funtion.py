18b68cea9a8f3dfccd49718461bf98c4d74a6615
Husnain <Husnain.tayab@hotmail.com>
2025-08-21 15:54:51 +0500

    Functions

---
+++ funtion.py
@@ -0,0 +1,16 @@
+
+def average ():
+    number1 = int(input("Enter your number :"))
+    number2 = int(input("Enter your number :"))
+    number3 = int(input("Enter your number :"))
+
+    average = (number1 +number2+number3)/3
+
+    print(average)
+average()
+average()
+average()
+average()
+average()
+average()
+
