09e4b8d4396477cf05dc8b54f78397c646e84d89
Husnain <Husnain.tayab@hotmail.com>
2025-08-22 14:47:55 +0500

    Write file

---
+++ write file.py
@@ -0,0 +1,17 @@
+
+string = """"Python is a high-level, general-purpose programming language created by Guido van Rossum in 1991.
+It is one of the most popular languages today because it is:
+
+✅ Easy to learn – simple English-like syntax
+✅ Versatile – used in web development, data science, artificial intelligence, automation, finance, etc.
+✅ Beginner-friendly – perfect for starting your programming journey
+✅ Large community – thousands of libraries and frameworks to make task"""
+
+
+
+
+
+
+file = open("my_file","w")
+file.write(string)
+file.close()
