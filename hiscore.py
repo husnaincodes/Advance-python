ebf1a90fea9beb6301c1c7ac3c64d7214e87837c
Husnain <Husnain.tayab@hotmail.com>
2025-08-22 16:06:14 +0500

    Hiscore Game

---
+++ hiscore.py
@@ -0,0 +1,32 @@
+
+import random
+
+def game():
+
+ print("You are playing the game... ")
+
+ score = random.randint(1,50)
+
+ with open("hiscore.txt") as file:
+
+    hiscore = file.read()
+
+    if(hiscore!=""):
+        hiscore = int(hiscore)
+
+    else:
+        hiscore = 0
+ print(f"Your score {score}")
+
+ if(score>hiscore):
+
+    with open ("hiscore.txt","w") as file :
+
+        file.write(str(score))
+
+ return score
+
+game()
+
+
+
