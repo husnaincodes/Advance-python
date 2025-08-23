7a99330757960e559dda0054ed5aa2b501fd7a57
Husnain <Husnain.tayab@hotmail.com>
2025-08-23 18:45:28 +0500

    Class

---
+++ class.py
@@ -0,0 +1,14 @@
+
+class Empolyee:
+    language = "python"
+    salary = 800000
+user = Empolyee()
+user.name = "USER"
+print(user.name,user.language,user.salary)
+
+
+
+user2 = Empolyee()
+user2.language = "java"
+user2.name = "USER2"
+print(user2.name,user2.language,user2.salary)
