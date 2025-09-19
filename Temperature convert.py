449b56336dbad89565cc9d5d937bd51288d40e2a
Husnain <Husnain.tayab@hotmail.com>
2025-09-19 21:50:45 +0500

    Temperature Convertor

--- Temperature convert.py
+++ Temperature convert.py
@@ -3,8 +3,8 @@ def fahrenheit_to_celsius(fahrenheit):
 
     return 5*(fahrenheit-32)/9
 
-fahrenheit = int(input("Enter the temperature :  "))
+fahrenheit_value = int(input("Enter the temperature :  "))
 
-round = round(fahrenheit_to_celsius(fahrenheit),2)
+round = round(fahrenheit_to_celsius(fahrenheit_value),2)
 
 print(f"{round}°C")
