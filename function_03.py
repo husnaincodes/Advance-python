a0f660cc81a47a59c22441e57b0c98c0ebcd1fcb
Husnain <Husnain.tayab@hotmail.com>
2025-10-03 19:28:35 +0500

    Function

--- function_03.py
+++ function_03.py
@@ -14,9 +14,7 @@ def greater (number1 ,number2, number3):
         return f"THE LARGEST NUMBER IS :{number3}"
     
 number1= int(input("Enter your number : "))
-
 number2= int(input("Enter your number : "))
-
 number3= int(input("Enter your number : "))
 
 print(greater(number1,number2,number3))
