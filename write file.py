e1c37d8ffb38091e1ec4267c5bb65baa18feb5a4
Husnain <Husnain.tayab@hotmail.com>
2025-10-11 19:59:47 +0500

    Write function

--- write file.py
+++ write file.py
@@ -2,7 +2,7 @@
 string = """"Python is a high-level, general-purpose programming language created by Guido van Rossum in 1991.
 It is one of the most popular languages today because it is:
 
-✅ Easy to learn – simple English-like syntax
+
 ✅ Versatile – used in web development, data science, artificial intelligence, automation, finance, etc."""
 
 
