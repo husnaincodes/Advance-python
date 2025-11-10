0e7ee534f622d4aef05fb47882c993815cee7397
Husnain <Husnain.tayab@hotmail.com>
2025-11-10 22:36:05 +0500

    Function Largest number

--- function_03.py
+++ function_03.py
@@ -3,13 +3,12 @@
 def greater (number1 ,number2, number3):
 
     if(number1>number2):
-
         return f"THE LARGEST NUMBER IS :{number1}"
+    
     elif(number2>number3):
      return f"THE LARGEST NUMBER IS :{number2}"
 
     elif(number3>number1):
-
         return f"THE LARGEST NUMBER IS :{number3}"
     
 number1= int(input("Enter your number : "))
