87740cd7e9013ac856d38fcbe62b0da52e3f068f
Husnain <Husnain.tayab@hotmail.com>
2026-01-01 21:59:20 +0500

    Write file program

--- write file.py
+++ write file.py
@@ -6,10 +6,6 @@ It is one of the most popular languages today because it is:
 ✅ Versatile – used in web development, data science, artificial intelligence,finance, etc."""
 
 
-
-
-
-
 file = open("my_file","w")
 file.write(string)
 file.close()
