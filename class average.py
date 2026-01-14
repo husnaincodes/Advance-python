29319312d43b41f61c96c2864e460e9764fbb755
Husnain <Husnain.tayab@hotmail.com>
2026-01-14 18:51:59 +0500

    Object Oriented Programming

--- class average.py
+++ class average.py
@@ -1,9 +1,12 @@
 class Student():
+    
     def __init__(self,name,marks):
         self.name = name
         self.marks = marks
+
     def average(self):
         avg = sum(self.marks)
         print(f"HI {self.name} your avg score is: {avg/3}")
+
 s1 = Student("Hunain",[98,88,85])
 s1.average()
