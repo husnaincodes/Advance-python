78487db9322c354524cffe8011dc9fbc6edce4fa
Husnain <Husnain.tayab@hotmail.com>
2026-01-16 23:16:16 +0500

    Account class

--- class account.py
+++ class account.py
@@ -3,4 +3,9 @@ class Account:
         
         self.balance = bal
         self.account_no = acc
-        
+    def debit(self,amount):
+        if self.balance<amount:
+            print("Error! Your balance is low!!")
+        else:
+            self.balance=-amount
+            print(f"RS {amount} was debited")
